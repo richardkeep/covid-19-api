15ff5e7212867107aaf5e491f4bfff6c51084c3e
Richard Keep <r.kipsang@gmail.com>
2020-03-25 13:40:04 +0300

    wip

--- app/Country.php
+++ app/Country.php
@@ -17,6 +17,32 @@ class Country extends Model
         'todayDeaths', 'recovered', 'activeCases', 'critical',
     ];
 
+    protected $mappings = [
+        'UK' => 'United Kingdom',
+        'S. Korea' => 'South Korea',
+        'N. Korea' => 'North Korea',
+        'USA' => 'United States',
+        'Hong Kong' => 'Hong Kong SAR China',
+        'UAE' => 'United Arab Emirates',
+        'Palestine' => 'Palestinian Territories',
+        'Bosnia and Herzegovina' => 'Bosnia & Herzegovina',
+        'North Macedonia' => 'Macedonia',
+        'Macao' => 'Macau SAR China',
+        'DRC' => 'Congo - Kinshasa',
+        'Congo' => 'Congo - Kinshasa',
+        'Saint Martin' => 'St. Martin',
+        'Saint Lucia' => 'St. Lucia',
+        'Trinidad and Tobago' => 'Trinidad & Tobago',
+        'Antigua and Barbuda' => 'Antigua & Barbuda',
+        'Ivory Coast' => 'Côte d’Ivoire',
+        'St. Vincent Grenadines' => 'St. Vincent & Grenadines',
+        'Faeroe Islands' => 'Faroe Islands',
+        'Turks and Caicos' => 'Turks & Caicos Islands',
+        'Myanmar' => 'Myanmar (Burma)',
+        'Cabo Verde' => 'Cape Verde',
+        'CAR' => 'Central African Republic',
+    ];
+
     protected $casts = [
         'todayCases' => 'integer',
         'todayDeaths' => 'integer',
@@ -34,6 +60,13 @@ class Country extends Model
         });
     }
 
+    private function generateEmoji($country)
+    {
+        $country = array_key_exists($country, $this->mappings) ? $this->mappings[$country] : $country;
+
+        return collect(json_decode(file_get_contents(__DIR__.'/flags.json'), true))->firstWhere('name', $country)['emoji'];
+    }
+
     private function getData()
     {
         $collection = collect($this->crawl());
@@ -46,6 +79,8 @@ class Country extends Model
                 // Replace array keys with titles
                 $data[$v] = str_replace(',', '', $item[$kk]);
             }
+
+            $data['emoji'] = $this->generateEmoji($item[0]);
 
             return $data;
         })
